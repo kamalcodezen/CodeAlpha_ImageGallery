4d1530590cc95e7f44eca2605bfda380150400bb
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-25 14:34:03 +0530

    feat(js): display clicked image in lightbox

--- js/script.js
+++ js/script.js
@@ -5,15 +5,24 @@ const lightboxImage = document.querySelector(".lightbox-image")
 const closeBtn = document.querySelector(".close-btn")
 const prevBtn = document.querySelector(".prev-btn")
 const nextBtn = document.querySelector(".next-btn")
+const item = document.querySelector(".gallery-item")
 
 
-
+// Lightbox open
 galleryItems[0].addEventListener("click", () => {
     lightbox.classList.add("active")
 })
 
-
+// Lightbox close
 closeBtn.addEventListener("click", () => {
     lightbox.classList.remove("active")
 })
 
+
+galleryItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        const image = item.querySelector("img")
+        lightboxImage.src = image.src
+        lightbox.classList.add("active")
+    })
+})
