f028ca0465383e1dca2aed355e31012c875b22ac
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-25 14:01:49 +0530

    feat(js): open lightbox on first image click

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const galleryItems = document.querySelectorAll('.gallery-item');
+const lightbox = document.querySelector(".lightbox")
+const lightboxImage = document.querySelector(".lightbox-image")
+const closeBtn = document.querySelector(".close-btn")
+const prevBtn = document.querySelector(".prev-btn")
+const nextBtn = document.querySelector(".next-btn")
+
+
+
+galleryItems[0].addEventListener("click", () => {
+    lightbox.classList.add("active")
+})
